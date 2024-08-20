e7e835bc46ec5bfafd3eba1e8c6e31981a4b0fa1
Douglas Berkley <douglasmberkley@gmail.com>
2024-08-20 18:51:17 +0900

    Added edit and destroy for batches

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,7 +1,32 @@
 class BatchesController < ApplicationController
   def show
     @batch = Batch.find(params[:id])
-    @batchs = Batch.order(number: :desc)
+    @batches = Batch.order(number: :desc)
     @message = Message.new
+  end
+
+  def edit
+    @batch = Batch.find(params[:id])
+  end
+
+  def update
+    @batch = Batch.find(params[:id])
+    if @batch.update(batch_params)
+      redirect_to batch_path(@batch)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @batch = Batch.find(params[:id])
+    @batch.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def batch_params
+    params.require(:batch).permit(:number, :start_date, :end_date, :slack_channel, :data_science, :part_time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :batches, only: :show do
+  resources :batches, only: [:show, :edit, :destroy] do
     resources :messages, only: :create
   end
 end
